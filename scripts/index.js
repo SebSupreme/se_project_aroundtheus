d413850c6398d6cabadb603ed0415a51386c0392
SebastianG <sebastian_gonzalez17@yahoo.com>
2024-08-05 11:47:28 -0400

    typo correction

--- scripts/index.js
+++ scripts/index.js
@@ -1,4 +1,4 @@
-const intialCards = [
+const initialCards = [
   {
     name: "Yosemite Valley",
     link: "https://practicum-content.s3.us-west-1.amazonaws.com/software-engineer/around-project/yosemite.jpg",
